9cf589b2307df5642822e5839ff0c5803245ad55
Kenny Wolfenberger <kwolfenberger@SEA-PRO-12.local>
2020-01-31 08:56:33 -0800

    update phone validation

--- dist/conversational-form.js
+++ dist/conversational-form.js
@@ -4308,28 +4308,30 @@ var cf;
             }));
         };
 
-        UserTextInput.prototype.phoneFormatUpdate = function(val) {
-            let num = val.replace(/-/,'')
-            console.log(num)
-            let new_number = val;
-            if (num.length > 2) {
+        UserTextInput.prototype.phoneFormatUpdate = function(val = '') {
+            if (!val) return val; //null check
+            const clean = ('' + val).replace(/\D/g, '');
+            console.log(clean)
+
+            let phone_number;
+            if (clean.length > 2) {
                 // matches: 123 || 123-4 || 123-45
-                new_number = num.substring(0,3) + '-';
-                if (num.length === 4 || num.length === 5) {
+                phone_number = clean.substring(0,3) + '-';
+                if (clean.length === 4 || clean.length === 5) {
                     // matches: 123-4 || 123-45
-                    new_number += num.substr(3);
+                    phone_number += num.substr(3);
                 }
-                else if (num.length > 5) {
+                else if (clean.length > 5) {
                     // matches: 123-456 || 123-456-7 || 123-456-789
-                    new_number += num.substring(3,6) + '-';
+                    phone_number += (num.substring(3,6) + '-' + num.substring(6));
                 }
-                if (num.length > 6) {
-                    // matches: 123-456-7 || 123-456-789 || 123-456-7890
-                    new_number += num.substring(6);
-                }
+                // if (clean.length > 6) {
+                //     // matches: 123-456-7 || 123-456-789 || 123-456-7890
+                //     new_number += num.substring(6);
+                // }
               } 
-            else new_number=num;
-            this.inputElement.value =  (new_number.length > 12) ? new_number.substring(0,12) : new_number;
+            else phone_number=clean;
+            this.inputElement.value =  (phone_number.length > 12) ? phone_number.substring(0,12) : phone_number;
 
             
             console.log(new_number);
